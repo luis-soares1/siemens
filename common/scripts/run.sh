c01529f7b11da16c1bcc1ad813a395db9b9373a2
luissoares1 <luis.soares@aspiretechnology.com>
2023-10-31 00:00:23 +0000

    deploy script improved -> does not run migrations when its not needed.

--- common/scripts/run.sh
+++ common/scripts/run.sh
@@ -3,6 +3,11 @@
 set -e
 
 cd /app/api
-alembic revision --autogenerate -m "First Migration - Setup"
-alembic upgrade head
+
+# Check if the directory contains any .py files
+if [ -z "$(ls alembic/versions/*.py 2>/dev/null)" ]; then
+    alembic revision --autogenerate -m "First Migration - Setup"
+    alembic upgrade head
+fi
+
 python main.py
